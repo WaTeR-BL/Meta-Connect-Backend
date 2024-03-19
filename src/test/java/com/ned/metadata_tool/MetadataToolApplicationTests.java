24b81a4aff91d3808a9954ce84a651ad161f629a
Abdullah Bin Musharraf <142393247+WaTeR-BL@users.noreply.github.com>
2024-03-19 10:54:19 +0500

    first commit

---
+++ src/test/java/com/ned/metadata_tool/MetadataToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ned.metadata_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadataToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
