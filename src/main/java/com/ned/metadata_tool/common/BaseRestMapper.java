1fb7c88f3e82066976b36ab8ce92737729085c23
Abdullah Bin Musharraf <142393247+WaTeR-BL@users.noreply.github.com>
2024-03-26 22:26:53 +0500

    access

--- src/main/java/com/ned/metadata_tool/common/BaseRestMapper.java
+++ src/main/java/com/ned/metadata_tool/common/BaseRestMapper.java
@@ -3,7 +3,7 @@ package com.ned.metadata_tool.common;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@CrossOrigin(origins = {"http://localhost*"}, maxAge = 3600, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:8080","http://localhost:5173"}, maxAge = 3600, allowCredentials = "true")
 @RequestMapping("/api/meta-connect")
 public interface BaseRestMapper {
 }
