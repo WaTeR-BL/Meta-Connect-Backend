0f77234fdc5ed02d38d10564ff86eb5d245536d8
Abdullah Bin Musharraf <142393247+WaTeR-BL@users.noreply.github.com>
2024-03-26 23:32:12 +0500

    access

--- src/main/java/com/ned/metadata_tool/configuration/SecurityConfig.java
+++ src/main/java/com/ned/metadata_tool/configuration/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.ned.metadata_tool.service.impl.UserAuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -43,8 +44,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/api/meta-connect/authenticate","/api/meta-connect/registration")
-                .permitAll().anyRequest().authenticated()
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+                .antMatchers("/api/meta-connect/authenticate","/api/meta-connect/registration")
+                .permitAll().anyRequest().permitAll()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);;
